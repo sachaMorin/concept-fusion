f3cf8dff252efb014fc4b5fc9aea655b5c831633
sachaMorin <morin.sacha@gmail.com>
2025-01-23 17:23:03 -0500

    Fix path in launch_replica script

--- slurm/launch_replica.sh
+++ slurm/launch_replica.sh
@@ -5,5 +5,5 @@ DATA_DIR=$SCRATCH/data/Replica
 
 for sequence in room0 room1 room2 office0 office1 office2 office3 office4
 do 
-    sbatch $HOME/concept-fusion/slurm/full_conceptfusion.sh $DATASET_NAME $DATA_DIR $sequence
+    sbatch $HOME/concept-fusion/concept-fusion/slurm/full_conceptfusion.sh $DATASET_NAME $DATA_DIR $sequence
 done
